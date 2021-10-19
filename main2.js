dd01b007aab25ab44116fc839f0ff5e8838dbf88
Diego Armando Beltran Saavedra <92826950+ingdiegoabeltran@users.noreply.github.com>
2021-10-19 16:23:16 -0500

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,69 @@
+// objetors literales
+
+const juan1 = {
+    name: "juandc",
+    username: "juandc",
+    points: 100,
+    socialMedia: {
+        twitter: "fjuandc",
+        instagram: "fjuandc",
+        facebook: undefined,
+    },
+    approvedCourses: [
+        "Curso definitivo html y css",
+        "curso practivo de html y css",
+    ],
+    learningPaths: [
+        {
+            name: "Escuela de desarrollo web",
+            courses: [
+                "Curso definitivo html y css",
+                "curso practivo de html y css",
+                "curso de responsive design",
+            ],
+        },
+
+        {
+            name: "Escuela de Videojuegos",
+            courses: [
+                "Curso de introduccion a la produccion de vgs",
+                "curso de unreal engine ",
+                "curso de unioty 3D",
+            ],
+        },
+    ],
+};
+
+const miguelito = {
+    name: "miguelito",
+    username: "miguelitofeliz",
+    points: 1000,
+    socialMedia: {
+        twitter: "miguelitofeliz",
+        instagram: "miguelito_feliz",
+        facebook: undefined,
+    },
+    approvedCourses: [
+        "Curso databusiness",
+        "curso dataviz",
+    ],
+    learningPaths: [
+        {
+            name: "Escuela de desarrollo web",
+            courses: [
+                "Curso definitivo html y css",
+                "curso practivo de html y css",
+                "curso de responsive design",
+            ],
+        },
+
+        {
+            name: "Escuela de science",
+            courses: [
+                "Curso databusiness",
+                "curso dataviz",
+                "curso tableau",
+            ],
+        },
+    ],
+};
